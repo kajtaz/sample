895b61dddff0014b06e7a1891035c477bc248cd7
kajtaz <grottelt@gmail.com>
2019-10-22 14:15:10 +0200

    add acc activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
+    redirect_to root_url
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the sample app"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your mailbox to confirm your account activation"
+      redirect_to root_url
     else
       render 'new'
     end
